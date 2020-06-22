ddb19d7966af3faf107092c8c64f7525a19d0df3
PTHParkTaeHwan <pth0810@naver.com>
2020-06-22 23:24:45 +0900

    Start

---
+++ Source/FlyingDrone/BasicCharacter.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicCharacter.h"
+
+// Sets default values
+ABasicCharacter::ABasicCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABasicCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasicCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABasicCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ABasicCharacter::LeftRight);
+
+}
+
+void ABasicCharacter::LeftRight(float NewAxisValue)
+{
+	AddMovementInput(FRotationMatrix(GetControlRotation()).GetUnitAxis(EAxis::Y), NewAxisValue);
+}
+
---
+++ Source/FlyingDrone/BasicCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BasicCharacter.generated.h"
+
+UCLASS()
+class FLYINGDRONE_API ABasicCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABasicCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//==============
+	//  movement  //
+	//==============
+
+protected:
+	void LeftRight(float NewAxisValue);
+
+
+};
